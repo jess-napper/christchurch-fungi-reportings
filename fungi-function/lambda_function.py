a1c53418824009fca93ded65dd0bdd974ef144c9
jess-napper <napper.jess@gmail.com>
2024-05-29 09:59:13 +1200

    Updates to lambda_function.py - logging added (closes Add basic logging to project #13), pagination added to api request to iNaturalist (closes #14) and sensitive information removed and stored in environment variables (closes Store sensitive information in Environment Variables #12). Updates to README to reflect changes to the function.

--- fungi-function/lambda_function.py
+++ fungi-function/lambda_function.py
@@ -4,34 +4,64 @@ import requests
 import pandas as pd
 import boto3
 import re
+import os
+import logging
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+log_file = '/tmp/execution_log.log'  # Temporary file in the Lambda environment
+file_handler = logging.FileHandler(log_file)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 def lambda_handler(event, context):
-    # Parse query parameters from the event
-    query_params = event.get('queryStringParameters', {})
-    endpoint = event.get('requestContext', {}).get("path") # returns the endpoint path
-    
-    # Check for API key
-    api_key = query_params.get('api_key', "")
-    if api_key != "Fish_Sea_Hat_Forest!":
+    logger.info("Lambda function started")
+    try:
+        # Parse query parameters from the event
+        query_params = event.get('queryStringParameters', {})
+        endpoint = event.get('requestContext', {}).get("path")  # returns the endpoint path
+        
+        # Check for API key
+        api_key = query_params.get('api_key', "")
+        if api_key != os.environ.get('API_KEY'):
+            logger.warning("Unauthorized access attempt with invalid API key")
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"error": "Unauthorised. Invalid API key."})
+            }
+        
+        if endpoint == '/metadata':
+            logger.info("Endpoint '/metadata' accessed")
+            # Return data from iNaturalist
+            response = get_metadata_table(query_params)
+        
+        elif endpoint == '/data':
+            logger.info("Endpoint '/data' accessed")
+            # Return metadata table
+            response = get_observation_data(query_params)
+        
+        else:
+            logger.error("Invalid endpoint accessed")
+            response = {
+                'statusCode': 400,
+                'body': 'Invalid endpoint'
+            }
+        
+        logger.info("Lambda function executed successfully")
+        return response
+
+    except Exception as e:
+        logger.exception("Exception occurred during Lambda execution")
         return {
-            "statusCode": 401,
-            "body": json.dumps({"error": "Unauthorised. Invalid API key."})
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
         }
-    
-    if endpoint == '/metadata':
-        # Return data from iNaturalist
-        return get_metadata_table(query_params)
-    
-    elif endpoint == '/data':
-        # Return metadata table
-        return get_observation_data(query_params)
-    
-    else:
-        # Handle if the endpoint is not recognized
-        response = {
-            'statusCode': 400,
-            'body': 'Invalid endpoint'
-        }
+
+    finally:
+        upload_log_to_s3()
 
 def transform_version(version):
     # Transform version from v1.0.0 to 1-0-0
@@ -46,7 +76,7 @@ def get_metadata_table(query_params):
     
     # Retrieve metadata from S3 based on the specified version
     s3 = boto3.client('s3')
-    bucket_name = 'data472-jkn35-metadata-fungi-observations'
+    bucket_name = os.environ.get('S3_BUCKET_NAME')
     prefix = 'metadata/metadata_v'  # Metadata is stored with names like "metadata_v1-0-0.json"
 
     if metadata_version == 'LATEST':
@@ -68,6 +98,7 @@ def get_metadata_table(query_params):
     try:
         response = s3.get_object(Bucket=bucket_name, Key=key)
         metadata_json = json.loads(response['Body'].read())
+        logger.info("Metadata retrieved successfully")
     except Exception as e:
         return {
             "statusCode": 500,
@@ -100,12 +131,14 @@ def get_observation_data(query_params):
             end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
         
         except ValueError:
+            logger.error("Invalid date format")
             return {
                 "statusCode": 400,
                 "body": json.dumps({"error": "Invalid date format. Please use YYYY-MM-DD."})
             }
         
         if end_date <= start_date:
+            logger.error("End date must be after start date")
             return {
                 "statusCode": 400,
                 "body": json.dumps({"error": "End date must be after start date."})
@@ -113,6 +146,7 @@ def get_observation_data(query_params):
         
     elif (start_date_str and not end_date_str) or (end_date_str and not start_date_str):
         # If only one date is given, state that both are required.
+        logger.error("Both start date and end date must be provided")
         return {
             "statusCode": 400,
             "body": json.dumps({"error": "End date and start date must both be given."})
@@ -136,33 +170,50 @@ def get_observation_data(query_params):
         "d2": end_date.isoformat(),
         "quality_grade": "research"  # Retrieve only verified observations
     }
+
+    logger.info(f"API input parameters: {params}")
     
-    # Make request to iNaturalist API
-    response = requests.get("https://api.inaturalist.org/v1/observations", params=params)
+    observations = []  # List to store all observations
     
-    # Process response data
-    if response.status_code == 200:
-        data = response.json().get('results', [])
-        df = process_data(data)
+    # Continue making requests until all pages are fetched
+    while True:
+        # Make request to iNaturalist API
+        response = requests.get("https://api.inaturalist.org/v1/observations", params=params)
         
-        # Convert DataFrame to CSV
-        csv_data = df.to_csv(index=False)
+        # Process  response data
+        if response.status_code == 200:
+            data = response.json().get('results', [])
+            observations.extend(data)  # Add observations from current page
+            
+            # Check if there is more than one page
+            if response.json().get('total_results', 0) > len(observations):
+                params['page'] += 1  # Move to the next page
+            else:
+                break  # No more pages, break the loop
         
-        # Prepare response
-        response_body = {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "text/csv",
-                "Content-Disposition": f"attachment; filename=inaturalist_observations.csv",
-                "metadata_version": "1.0.0",  # Metadata version information here
-            },
-            "body": csv_data,
-        }
-    else:
-        response_body = {
-            "statusCode": response.status_code,
-            "body": response.text
-        }
+        else:
+            logger.error(f"Error retrieving observation data: {response.status_code}")
+            return {
+                "statusCode": response.status_code,
+                "body": response.text
+            }
+    
+    df = process_data(observations)
+    
+    # Convert DataFrame to CSV
+    csv_data = df.to_csv(index=False)
+    logger.info(f"Observation data retrieved successfully. Number of observations: {len(observations)}")
+    
+    # Prepare response
+    response_body = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/csv",
+            "Content-Disposition": f"attachment; filename=inaturalist_observations.csv",
+            "metadata_version": "1.0.0",  # Metadata version information here
+        },
+        "body": csv_data,
+    }
     
     return response_body
 
@@ -190,3 +241,32 @@ def process_data(data):
 
     df = pd.DataFrame(observations)
     return df
+
+def upload_log_to_s3():
+    try:
+        s3 = boto3.client('s3')
+        bucket_name = os.environ.get('S3_BUCKET_NAME')
+        log_prefix = 'logs/execution_log.log'
+        
+        # Read existing log data from S3
+        existing_log_data = ""
+        try:
+            obj = s3.get_object(Bucket=bucket_name, Key=log_prefix)
+            existing_log_data = obj['Body'].read().decode('utf-8')
+        except s3.exceptions.NoSuchKey:
+            # If the log file doesn't exist yet, ignore the error
+            pass
+        
+        # Append new log data to existing log data
+        with open(log_file, 'r') as new_log_file:
+            new_log_data = new_log_file.read()
+        
+        combined_log_data = existing_log_data + new_log_data
+        
+        # Upload combined log data to S3
+        s3.put_object(Bucket=bucket_name, Key=log_prefix, Body=combined_log_data.encode('utf-8'))
+        
+        logger.info("Log file uploaded to S3 successfully")
+    except Exception as e:
+        logger.error(f"Error uploading log file to S3: {str(e)}")
+
